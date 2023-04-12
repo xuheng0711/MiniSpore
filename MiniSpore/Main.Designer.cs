cdd64e17738d6486ef41ee3d1313abd41e835c32
okq-xuheng <xuheng0711@163.com>
2023-04-12 14:11:54 +0800

    11111

---
+++ MiniSpore/Main.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace MiniSpore
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbMin = new System.Windows.Forms.PictureBox();
+            this.pbClose = new System.Windows.Forms.PictureBox();
+            this.gbParameter = new System.Windows.Forms.GroupBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.gbDeviceMessage = new System.Windows.Forms.GroupBox();
+            this.panelMain.SuspendLayout();
+            this.panelTitle.SuspendLayout();
+            this.panelContent.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelMain
+            // 
+            this.panelMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelMain.Controls.Add(this.panelContent);
+            this.panelMain.Controls.Add(this.panelBottom);
+            this.panelMain.Controls.Add(this.panelTitle);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(1008, 729);
+            this.panelMain.TabIndex = 0;
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(114)))), ((int)(((byte)(217)))));
+            this.panelTitle.Controls.Add(this.lblTitle);
+            this.panelTitle.Controls.Add(this.pbClose);
+            this.panelTitle.Controls.Add(this.pbMin);
+            this.panelTitle.Controls.Add(this.pictureBox1);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(0, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(1006, 77);
+            this.panelTitle.TabIndex = 0;
+            this.panelTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitle_MouseDown);
+            this.panelTitle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelTitle_MouseMove);
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(114)))), ((int)(((byte)(217)))));
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom.Location = new System.Drawing.Point(0, 687);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Size = new System.Drawing.Size(1006, 40);
+            this.panelBottom.TabIndex = 1;
+            // 
+            // panelContent
+            // 
+            this.panelContent.BackColor = System.Drawing.SystemColors.Control;
+            this.panelContent.Controls.Add(this.gbParameter);
+            this.panelContent.Controls.Add(this.gbDeviceMessage);
+            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContent.Location = new System.Drawing.Point(0, 77);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(1006, 610);
+            this.panelContent.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MiniSpore.Properties.Resources.pictureBox1_Image;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(61, 65);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbMin
+            // 
+            this.pbMin.Image = global::MiniSpore.Properties.Resources.pictureBox2_Image;
+            this.pbMin.Location = new System.Drawing.Point(926, 10);
+            this.pbMin.Name = "pbMin";
+            this.pbMin.Size = new System.Drawing.Size(24, 24);
+            this.pbMin.TabIndex = 2;
+            this.pbMin.TabStop = false;
+            this.pbMin.Click += new System.EventHandler(this.pbMin_Click);
+            // 
+            // pbClose
+            // 
+            this.pbClose.Image = global::MiniSpore.Properties.Resources.pictureBox3_Image;
+            this.pbClose.Location = new System.Drawing.Point(966, 10);
+            this.pbClose.Name = "pbClose";
+            this.pbClose.Size = new System.Drawing.Size(24, 24);
+            this.pbClose.TabIndex = 3;
+            this.pbClose.TabStop = false;
+            this.pbClose.Click += new System.EventHandler(this.pbClose_Click);
+            // 
+            // gbParameter
+            // 
+            this.gbParameter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbParameter.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold);
+            this.gbParameter.ForeColor = System.Drawing.Color.Green;
+            this.gbParameter.Location = new System.Drawing.Point(0, 404);
+            this.gbParameter.Name = "gbParameter";
+            this.gbParameter.Size = new System.Drawing.Size(1006, 206);
+            this.gbParameter.TabIndex = 1;
+            this.gbParameter.TabStop = false;
+            this.gbParameter.Text = "设备参数配置";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("宋体", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(93, 18);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(550, 40);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "迷你智能孢子捕捉预测分析仪";
+            // 
+            // gbDeviceMessage
+            // 
+            this.gbDeviceMessage.Dock = System.Windows.Forms.DockStyle.Top;
+            this.gbDeviceMessage.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold);
+            this.gbDeviceMessage.ForeColor = System.Drawing.Color.Green;
+            this.gbDeviceMessage.Location = new System.Drawing.Point(0, 0);
+            this.gbDeviceMessage.Name = "gbDeviceMessage";
+            this.gbDeviceMessage.Size = new System.Drawing.Size(1006, 404);
+            this.gbDeviceMessage.TabIndex = 0;
+            this.gbDeviceMessage.TabStop = false;
+            this.gbDeviceMessage.Text = "设备信息";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.panelMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "迷你孢子仪";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.panelMain.ResumeLayout(false);
+            this.panelTitle.ResumeLayout(false);
+            this.panelTitle.PerformLayout();
+            this.panelContent.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClose)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.Panel panelContent;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbMin;
+        private System.Windows.Forms.PictureBox pbClose;
+        private System.Windows.Forms.GroupBox gbParameter;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.GroupBox gbDeviceMessage;
+    }
+}
+
---
+++ MiniSpore/Main.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniSpore
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        private void pbMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void pbClose_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+
+        //窗口移动
+        Point mPoint;
+        private void panelTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            mPoint = new Point(e.X, e.Y);
+        }
+
+        private void panelTitle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Location = new Point(this.Location.X + e.X - mPoint.X, this.Location.Y + e.Y - mPoint.Y);
+            }
+        }
+
+
+    }
+}
